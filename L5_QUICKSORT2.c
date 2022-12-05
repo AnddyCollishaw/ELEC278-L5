c37d1e48966bbc421ca8e70264c0ad695337b9ac
Andy Collishaw <andycolla03@gmail.com>
2022-12-05 15:10:32 -0500

    first commit

---
+++ L5_QUICKSORT2.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+//#include "L5_TESTSORT.c"
+
+// void printarrowatposition2 (int posn)
+// // Prints a V - a down arrow - where the pivot value will be
+// {
+// 	int	i;
+// 	for (i=0; i<posn; i++)		printf ("     ");
+// 	printf ("   V\n");
+// }//printarrowatposition2()
+
+int counter = 0; 
+
+int partution2 (int  *a,  int left, int right)
+// Using locations a[left..right], partution2 fragment by picking a[left] as
+// separator value, then working through bottom and top halves of the array,
+// arranging - by swapping values if necessary - that all values smaller than
+// the separator value are in the bottom part and all values larger are in
+// the top part.
+{
+	int	ll, rr, pivotval;
+	ll = left+1;
+	rr = right;
+	// choose pivot to be leftmost location
+	pivotval = a[left];
+	if (verbose)
+		printf ("partution2 around value in position %d - %d\n", left, a[left]);
+    while (ll < rr)	{
+		// if one on left less than pivot, leave it alone
+		if(a[ll] <= pivotval)	{ ll++; continue;}
+		// if one on right greater than pivot, leave it
+        if(a[rr] > pivotval)	{ rr--; continue;}
+		// both left and right on wrong side - swap them
+		swap(a, ll, rr);
+       	}//endwhile
+	// we stop when rr and ll collide. Place pivot value such that everything
+	// to left is less and everything to right is same or greater.
+	if (a[ll] < pivotval)	{
+		swap(a, ll, left);
+	} else	{
+		swap(a, --ll, left);
+		}
+	return ll;
+}//partution2()
+
+
+int quicksort2 (int *a,  int left, int right)
+{
+	
+	if (verbose)
+		printf ("\nQuicksort: left = %d   right = %d\n", left, right);
+	if (left < right)    {
+		int  pivotndx = partution2 (a, left, right);
+		if (verbose)	{
+			printf ("    Completed partution2, pivot at: %d\n", pivotndx);
+			// printarrowatposition2 (pivotndx);
+			printarray (NULL, a, 0, 19, 1000);
+			}
+		quicksort2 (a, left, pivotndx-1);
+		quicksort2 (a, pivotndx+1, right);
+		}
+	
+
+	
+	return swaps;
+}//quicksort()
+
+int insertionsort2 (int *a, int min, int max)
+// Work through array of numbers, placing each one in the correct place. Note
+// that insertion sort differs from bubblesort in that with insertion sort, one
+// value is moved to its relative place. In bubble sort, the largest value is
+// found as the checking keeps choosing the larger of two values to keep
+// moving.
+{
+	int		i, j;
+	int		rslt = -1;
+
+	// Check for errors
+	if (a == NULL || (min > max))	{
+		error ("insertionsort() called with invalid parameters");
+	} else	{
+		// initialize swaps so we know how many exchanges take place
+		
+
+		// Outer for loop works through values from min to max. Effectively,
+		// we start by making the data in position relative 0 a sorted array containing
+		// one element, then adding the rest of the array, one by one, to the
+		// sorted part
+		for (i = min; i <=max; i++) {
+			// Now, i indicates which new element in the array is going to be
+			// inserted into its correct position. At the end of each loop, one
+			// more value is moved, and all of the bottom is ordered.
+			for (j = i; j > min && (a[j - 1] > a[j]); j--) {
+				swap(a, j, j - 1);
+				}//endfor
+			}//endfor
+		rslt = swaps;
+		}//endif
+	return rslt;
+}//insertionsort()
+
+int quicksorthybrid(int *arr, int Left, int Right)
+{
+ 
+  int rslt = -1; 
+  
+  while (Left < Right)
+    {
+		
+		//printf("\n%d\n", swaps); 
+    // If the size of the array is less
+    // than threshold apply insertion sort
+    // and stop recursion
+	
+    if (Right-Left + 1 < 30 )
+      {
+        insertionsort2(arr, Left, Right);
+      break;
+    }
+ 
+    else
+       
+        {
+          int pivot = partution2(arr, Left, Right) ;
+ 
+      // Optimised quicksort which works on
+      // the smaller arrays first
+ 
+      // If the left side of the pivot
+      // is less than right, sort left part
+      // and move to the right part of the array
+	    
+      if (pivot-Left<Right-pivot)
+        {
+          quicksorthybrid(arr, Left, pivot - 1);
+        Left = pivot + 1;
+      }
+      else
+        {
+ 
+        // If the right side of pivot is less
+        // than left, sort right side and
+        // move to the left side
+           
+        quicksorthybrid(arr, pivot + 1, Right);
+        Right = pivot-1;
+        }
+ 
+     }
+ 
+   }
+   
+   rslt = swaps; 
+   return rslt; 
+}
---
+++ L5_TESTSORT.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <time.h>
+
+
+
+
+// Array containing initial data
+		// how much actual data
+int data [100]	= {
+	44, 22,  3, 71, 16, 82,  1,  9, 99, 10,
+	32, 18, 77, 28,  9, 17, 52, 61, 38, 12 };
+
+// "Global" variables
+int					verbose = 0;
+long unsigned int	swaps = 0;			// may be lots of swaps
+
+#include "L5_UTILITYCODE.c"
+
+#include "L5_QUICKSORT2.c"
+
+#define MAXINDEX   =   19;
+
+int main (void)
+{
+	int			rslt, max, rslt1;
+	clock_t		start, mid, end;
+	int			*pa, *pa1;			// pointer to array;
+	int			size, size1;
+
+	// Fetch data
+	size = read_int_file ("RANDOM.TXT", &pa);
+	size1 = read_int_file("RANDOM.TXT", &pa1);
+	//size = read_int_file ("ASCENDING.TXT", &pa);
+	//size = read_int_file ("DESCENDING.TXT", &pa);
+	//return 0;
+
+	if (verbose)
+		printarray ("Array before any work done:", pa, 0, size-1, 10);
+	printf ("==Check array before sort\n");
+	if (array_sorted(NULL, pa, 0, size-1, 1))	{
+		printf ("Warning: Array is initially sorted\n");
+	} else	{
+		printf ("    Array is unsorted\n");
+		}
+	swaps = 0;
+	
+	start = clock();
+	//printf ("Bubble Sort\n");		rslt = bubblesort (pa, 0, size-1);           //OK
+	//printf ("Insertion Sort\n");	rslt = insertionsort (pa, 0, size-1);        //OK
+	//printf ("Heap Sort\n");		rslt = heapsort (pa, size);      //2B tested
+	//printf ("Merge Sort\n");		rslt = mergesort (pa, 0, size-1);            //OK
+	printf ("Quicksort\n");		 rslt1 = quicksort2 (pa1, 0, size-1);
+	mid = clock(); 
+	printf ("Quicksort2\n");		 rslt = quicksorthybrid (pa, 0, size-1);    
+	
+	
+	// Note: Radix sort does not need to be included in the comparisons
+	//printf ("Radix sort\n");		rslt = radixsort (pa, size);			 	 //OK
+	//printf ("Selection Sort (Iterative)\n");	rslt = selsortI (pa, 0, size-1); //OK
+	//printf ("Selection Sort (Recursive)\n");  rslt = selsortR (pa, 0, size-1); //OK
+	//printf ("Shell Sort\n");		rslt = ShellSort (pa, 0, size-1);            //OK
+	end = clock();
+	
+	// printf ("End time: %ld  Start time:  %ld  Diff: %ld\n",
+	// 			end, start, end-start);
+	// if (verbose || 1)
+	// 	printarray ("Final array:", pa, 0, size-1, 8);
+	// if (array_sorted(NULL, pa, 0, size-1, 1))
+	// 	printf ("==After sort, array is sorted\n");
+	// else	{
+	// 	printf ("Sort failed - array is not sorted\n");
+	// 	//printarray ("Sort error: ", pa, 0, size-1, 8);
+	// 	}
+	// printf ("Swaps:  %ld  CPU time:  %ld\n", rslt, end-start);
+
+
+
+
+
+FILE *fp; 
+
+    fp = fopen("LAB5_PART3.TXT", "w");
+
+    if(fp == NULL){
+        printf("\nCouldn't Open File\n");
+        return -1;
+	}
+
+	fprintf(fp,"ollishaw, Andy - Kung, Matthew  Size of Dataset: %d\n", size);
+	fprintf(fp,"                   Elapsed Time      Number of Swaps\n"); 
+	fprintf(fp,"Original Quicksort: %d               %d\n", mid-start, rslt1);
+	fprintf(fp,"Hyprid Quicksort:   %d               %d", end-mid, rslt);
+
+	 
+	return 0;
+}//main()
+
+
